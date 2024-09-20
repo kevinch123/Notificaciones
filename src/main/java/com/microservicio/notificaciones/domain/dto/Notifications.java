7560a3242dbd55de96037ed96d11712d6bafff1b
zare <zarellaburbanov@gmail.com>
2024-09-19 19:04:50 -0500

    dtos

--- src/main/java/com/microservicio/notificaciones/domain/dto/Notifications.java
+++ src/main/java/com/microservicio/notificaciones/domain/dto/Notifications.java
@@ -1,26 +1,8 @@
 package com.microservicio.notificaciones.domain.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "notifications")
 public class Notifications {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "notification_id")
-    private Long notificationId;
 
     private String header;
     private String content;
@@ -32,107 +14,70 @@ public class Notifications {
     private LocalDateTime readDate;
     private LocalDateTime deliveryDate;
     private LocalDateTime updateDate;
-
-    // OneToMany relationship with Preferences
-    @OneToMany(mappedBy = "notification", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Preferences> preferences;
-
-    // Getters and Setters
-
-    public Long getNotificationId() {
-        return notificationId;
-    }
-
-    public void setNotificationId(Long notificationId) {
-        this.notificationId = notificationId;
-    }
-
+    
     public String getHeader() {
         return header;
     }
-
     public void setHeader(String header) {
         this.header = header;
     }
-
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
-
     public String getNotificationType() {
         return notificationType;
     }
-
     public void setNotificationType(String notificationType) {
         this.notificationType = notificationType;
     }
-
     public String getRecipient() {
         return recipient;
     }
-
     public void setRecipient(String recipient) {
         this.recipient = recipient;
     }
-
     public int getPriority() {
         return priority;
     }
-
     public void setPriority(int priority) {
         this.priority = priority;
     }
-
     public int getStatus() {
         return status;
     }
-
     public void setStatus(int status) {
         this.status = status;
     }
-
     public LocalDateTime getCreationDate() {
         return creationDate;
     }
-
     public void setCreationDate(LocalDateTime creationDate) {
         this.creationDate = creationDate;
     }
-
     public LocalDateTime getReadDate() {
         return readDate;
     }
-
     public void setReadDate(LocalDateTime readDate) {
         this.readDate = readDate;
     }
-
     public LocalDateTime getDeliveryDate() {
         return deliveryDate;
     }
-
     public void setDeliveryDate(LocalDateTime deliveryDate) {
         this.deliveryDate = deliveryDate;
     }
-
     public LocalDateTime getUpdateDate() {
         return updateDate;
     }
-
     public void setUpdateDate(LocalDateTime updateDate) {
         this.updateDate = updateDate;
     }
 
-    public List<Preferences> getPreferences() {
-        return preferences;
-    }
+    
 
-    public void setPreferences(List<Preferences> preferences) {
-        this.preferences = preferences;
-    }
+
 }
 
